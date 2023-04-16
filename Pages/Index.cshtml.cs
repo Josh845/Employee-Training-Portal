14e289d2d8ae2c5e5db0aa652c43708282ca6c3d
Josh845 <lone5869@fredonia.edu>
2023-04-16 16:24:22 -0400

    authentication created; working with employer view now 4/16/23

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,5 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Employee_Training_Portal.Data;
+using Employee_Training_Portal.Model;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -10,49 +15,69 @@ namespace Employee_Training_Portal.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        //  private readonly ILogger<IndexModel> _logger;
+        /* public IndexModel(ILogger<IndexModel> logger)
+         {
+             _logger = logger;
+         }
+         */
 
-        public IndexModel(ILogger<IndexModel> logger)
+        private readonly ApplicationDbContext _db;
+        public IndexModel(ApplicationDbContext db)
         {
-            _logger = logger;
+            _db = db ?? throw new ArgumentNullException(nameof(db));
         }
 
         [BindProperty]
-        public string userpass { get; set; }    //get username 
+            public string user_name { get; set; }    //get username from html form
 
-        [BindProperty]
-        public string password { get; set; } //get user password
+            [BindProperty]
+            public string pass_word { get; set; } //get user password from html form
+        
         public void OnGet()
         {
-                    //method for first loading into page
-
+            
         }
 
         //function used to route to employee view
-        public async Task<IActionResult> OnPost(string user, string pass) {
-            //basic authentication
-            try
-            {              
-                if (user == "j" && pass == "pass")
-                {
-                    Console.WriteLine("Authenticated Employer");
-                    return RedirectToPage("Employer");
-                }
-                else if (user == "employee1" && pass == "test")
-                {
-                    Console.WriteLine("Authenticated Employee");
-                    return RedirectToPage("Employee");
-                }
-                else { Console.WriteLine("Authentication Failed"); }
+        public async Task<IActionResult> OnPost() {
+            //basic authentication 
+                    try
+                    {
+              
+                _db.Database.OpenConnection();
+                var userEmployeeName = _db.Employee.FirstOrDefault().fname.ToString().TrimEnd(); //gets employee name from db and trims
+                var userEmployerName = _db.Employer.FirstOrDefault().fname.ToString().TrimEnd(); //gets employer name from db and trims
 
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-                return RedirectToPage("Error");
-            }
+                var userEmployee_pass = _db.Employee.FirstOrDefault().password.ToString().TrimEnd(); //
+                var userEmployer_pass = _db.Employer.FirstOrDefault().password.ToString().TrimEnd();
 
-            return RedirectToPage("Employee");
+                if (userEmployerName == user_name && userEmployer_pass  == pass_word)
+                        {
+                            Console.WriteLine("Authenticated Employer");
+                            return RedirectToPage("Employer");
+                        }
+                        else if (userEmployeeName == user_name && userEmployee_pass == pass_word)
+                        {
+                            Console.WriteLine("Authenticated Employee");
+                            return RedirectToPage("Employee");
+                       }
+                
+                        else
+                        {
+                            Console.WriteLine("Authentication Failed");
+                            return RedirectToPage("Index");
+                        }
+
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                        return RedirectToPage("Error");
+                    }
+
+                    _db.Database.CloseConnection();
+                    return RedirectToPage("Index");
         }
 
     }
